a343cfbae0f77db0c438f4b45b0eb8dce71d2e4d
a7l7a7n <125312711+a7l7a7n@users.noreply.github.com>
2023-03-14 10:16:37 +1300

    update

---
+++ movie_ticket_v1.py
@@ -0,0 +1,17 @@
+"""Movie Theatre ticketing system
+Welcome screen and variables
+Created by Alan, although he has no idea how to do squat using Pycharm"""
+
+
+def sell_tickets():
+    print( "********** Fanfare Movies - ticketing system ***********\n")
+
+    adult_tickets = 0
+    child_ticket = 0
+    student_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sales = 0
+
+
+sell_tickets()
