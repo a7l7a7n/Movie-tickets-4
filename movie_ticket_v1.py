91762d0f41071a8bd07fdc7b58ab20b9ee8db12e
a7l7a7n <125312711+a7l7a7n@users.noreply.github.com>
2023-03-15 11:18:41 +1300

    update

--- movie_ticket_v1.py
+++ movie_ticket_v1.py
@@ -1,8 +1,12 @@
 """Movie Theatre ticketing system
-Welcome screen and variables
+Get the category and number of tickets required
 Created by Alan, although he has no idea how to do squat using Pycharm"""
 
 
+
+
+
+# Component 1 - Welcome screen and set up variables
 def sell_tickets():
     print( "********** Fanfare Movies - ticketing system ***********\n")
 
@@ -14,4 +18,20 @@ def sell_tickets():
     total_sales = 0
 
 
-sell_tickets()
+    #Component 2 - Get the category and number of tickets required
+
+    ticket_wanted = "Y"
+    while ticket_wanted == "Y":
+        ticket_type = input("What kind of ticket do you want: ")
+                            "\t'A' for Adult, or\n"
+                            "\t's' for Student, or\n"
+                            "\t'C' for Child, or\n"
+                            "\t'G' for Gift, or \n"
+                            ">> ") .upper()
+        num_tickets = int(input(f"How many {ticket_type} tickets do you want: "
+                                f""))
+        print(f"\nYou have ordered {num_tickets}{ticket_type} ticket(s)!")
+        ticket_wanted = input("Do you want to buy another ticket? (Y/N)")
+
+    # Main routine
+    sell_tickets()
